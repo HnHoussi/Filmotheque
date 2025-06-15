5e4704491a5c328f59b4aad39aecc10f3ac30842
Houcine <houcinehannoun@gmail.com>
2025-06-15 20:10:46 +0200

    creat the controller

--- src/main/java/fr/eni/ecole/filmotheque/controller/FilmController.java
+++ src/main/java/fr/eni/ecole/filmotheque/controller/FilmController.java
@@ -1,4 +1,28 @@
 package fr.eni.ecole.filmotheque.controller;
 
+import fr.eni.ecole.filmotheque.bll.FilmService;
+import fr.eni.ecole.filmotheque.bo.Film;
+
+import java.util.List;
+
 public class FilmController {
+    private FilmService filmService;
+
+    // injection with constructor
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    public void afficherFilms() {
+
+        System.out.println("Tous les film");
+        List<Film> films = filmService.consulterFilms();
+        films.forEach(System.out::println);
+    }
+
+    public void afficherFilmParId(long id) {
+        System.out.println("Film Par Id");
+        Film film = filmService.consulterFilmParId(id);
+        System.out.println(film);
+    }
 }
