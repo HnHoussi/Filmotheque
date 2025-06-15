85bb7f3fb648ca1432812d28c76aa5363b48e683
Houcine <houcinehannoun@gmail.com>
2025-06-15 23:27:28 +0200

    Add IHM templates

--- src/main/java/fr/eni/ecole/filmotheque/controller/FilmController.java
+++ src/main/java/fr/eni/ecole/filmotheque/controller/FilmController.java
@@ -3,6 +3,9 @@ package fr.eni.ecole.filmotheque.controller;
 import fr.eni.ecole.filmotheque.bll.FilmService;
 import fr.eni.ecole.filmotheque.bo.Film;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -10,21 +13,43 @@ import java.util.List;
 public class FilmController {
     private FilmService filmService;
 
-    // injection with constructor
+    //injection with constructor
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
 
-    public void afficherFilms() {
+    @GetMapping("/index.html")
+    public String homePage() {
 
-        System.out.println("Tous les film");
+        return "index";
+    }
+
+    @GetMapping("/films")
+    public String filmsPage(Model model) {
         List<Film> films = filmService.consulterFilms();
-        films.forEach(System.out::println);
+        model.addAttribute("films", films);
+
+        return "films";
+    }
+    @GetMapping("/films/details")
+    public String detailsPage(@RequestParam long id, Model model) {
+        Film film = filmService.consulterFilmParId(id);
+        model.addAttribute("film", film);
+
+        return "filmDetails";
     }
 
-    public void afficherUnFilm(long id) {
-        System.out.println("Film Par Id");
-        Film film = filmService.consulterFilmParId(id);
-        System.out.println(film);
-    }
+//    public void afficherFilms() {
+//
+//        System.out.println("Tous les film");
+//        List<Film> films = filmService.consulterFilms();
+//        films.forEach(System.out::println);
+//    }
+//
+//    public void afficherUnFilm(long id) {
+//        System.out.println("Film Par Id");
+//        Film film = filmService.consulterFilmParId(id);
+//        System.out.println(film);
+//    }
+
 }
